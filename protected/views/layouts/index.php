5cd50c7ff9f4e17129ab4c3b95d3b1677f7238c4
k.sidenko <k.sidenko@2gis.ru>
2014-02-16 00:41:10 +0700

    fix encode counter

--- protected/views/layouts/index.php
+++ protected/views/layouts/index.php
@@ -59,39 +59,39 @@
             <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo_small.png" alt="Шар" width="222" height="15" />
         </div>
 	<div class="metrika" >
-	<!-- Yandex.Metrika informer -->
-	<a href="http://metrika.yandex.ru/stat/?id=23965012&amp;from=informer"
-	target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/23965012/1_0_999387FF_797367FF_0_pageviews"
-	style="width:80px; height:15px; border:0;" alt="Яндек�.�е��ика" title="Яндек�.�е��ика: данн�е за �егодн� (п�о�мо���)" /></a>
-	<!-- /Yandex.Metrika informer -->
-	
-	<!-- Yandex.Metrika counter -->
-	<script type="text/javascript">
-	(function (d, w, c) {
-	(w[c] = w[c] || []).push(function() {
-	try {
-	w.yaCounter23965012 = new Ya.Metrika({id:23965012,
-	webvisor:true,
-	clickmap:true,
-	trackLinks:true,
-	accurateTrackBounce:true});
-	} catch(e) { }
-	});
-	
-	var n = d.getElementsByTagName("script")[0],
-	s = d.createElement("script"),
-	f = function () { n.parentNode.insertBefore(s, n); };
-	s.type = "text/javascript";
-	s.async = true;
-	s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-	
-	if (w.opera == "[object Opera]") {
-	d.addEventListener("DOMContentLoaded", f, false);
-	} else { f(); }
-	})(document, window, "yandex_metrika_callbacks");
-	</script>
-	<noscript><div><img src="//mc.yandex.ru/watch/23965012" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	<!-- /Yandex.Metrika counter -->
+        <!-- Yandex.Metrika informer -->
+        <a href="http://metrika.yandex.ru/stat/?id=23965012&amp;from=informer"
+           target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/23965012/1_0_999387FF_797367FF_0_pageviews"
+                                               style="width:80px; height:15px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" /></a>
+        <!-- /Yandex.Metrika informer -->
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter23965012 = new Ya.Metrika({id:23965012,
+                            webvisor:true,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true});
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="//mc.yandex.ru/watch/23965012" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
 	</div>
     </div><!-- .l-footer -->
 
