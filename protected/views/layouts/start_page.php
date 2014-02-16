1423f7127041a6ddf1490f2ddf20d4a517c1497e
k.sidenko <k.sidenko@2gis.ru>
2014-02-16 18:10:07 +0700

    add links on main page

--- protected/views/layouts/start_page.php
+++ protected/views/layouts/start_page.php
@@ -16,6 +16,25 @@
          background: #797367 url(/images/loader_background.jpg);
      }
 </style>
+
+<?
+    $links = array();
+    $links []= array("text" => 'АРХИТЕКТУРНОЕ ПРОЕКТИРОВАНИЕ', "link" => Yii::app()->createUrl('house'));
+    $links []= array("text" => 'ДИЗАЙН ПРОЕКТИРОВАНИЕ ИНТЕРЬЕРОВ ЗАГОРОДНЫХ ДОМОВ И КОТТЕДЖЕЙ', "link" => Yii::app()->createUrl('house'));
+    $links []= array("text" => 'ДИЗАЙН ПРОЕКТИРОВАНИЕ ИНТЕРЬЕРОВ КВАРТИР', "link" => Yii::app()->createUrl('flat'));
+    $links []= array("text" => 'ДИЗАЙН ПРОЕКТИРОВАНИЕ ОБЩЕСТВЕННЫХ ИНТЕРЬЕРОВ И ОФИСОВ', "link" => Yii::app()->createUrl('society'));
+    $links []= array("text" => 'РАЗРАБОТКА ФИРМЕННОГО СТИЛЯ', "link" => Yii::app()->createUrl('corp_style'));
+
+    $s = array();
+    foreach($links as $row) {
+        $s []= CHTML::link(mb_strtolower($row['text'], 'UTF8'), $row['link'], array('style' => 'font-size:10px; color:#797367;'));
+    }
+?>
+
+<div style="left:10px; color:#797367; position: relative; " >
+    <?php echo implode($s, ' | '); ?>
+</div>
+
 <div class="l-wrapper-loader">
 	<div class="loader">
 	   <img class="big_logo" src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo_big.png" alt="Шар" width="500" height="500"  />
